808effba5832361a4642da0fd4504445594d9fe5
sangosteve <stevesango9@gmil.com>
2023-02-09 16:38:34 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <Button variant="ghost" intent="primary">
+      <Button variant="outline">
         Button
       </Button>
     </div>
