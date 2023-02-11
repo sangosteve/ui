0439c426a70999fdcdf91229540bf3032c0b01bd
sangosteve <stevesango9@gmil.com>
2023-02-11 19:12:43 +0200

    added width and reuired to input

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "./components/ui/ButtonTemplate";
 import styles from "./page.module.css";
 import { Input } from "./components/ui/Input";
+import { Menu, MenuButton, MenuItem, MenuItems } from "./components/ui/MenuOld";
 export default function Home() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <Button variant="primary">Button</Button>
+      <Input placeholder="Enter email"  label="Email" required />
+      
     </div>
   );
 }
