451a383b51d21333c8259811752a0ed28bb8c2c3
sangosteve <stevesango9@gmil.com>
2023-02-12 19:09:03 +0200

    Tab

---
+++ app/components/ui/Tab.tsx
@@ -0,0 +1,83 @@
+"use client"
+import React from "react";
+import { Tab as HeadlessTab } from '@headlessui/react'
+import clsx from "clsx";
+
+
+
+interface TabGroupProps{
+    children?:React.ReactNode;
+}
+
+export function TabGroup({children}:TabGroupProps){
+    return(
+        <HeadlessTab.Group as="div" className="w-full text-body">
+            {children}
+        </HeadlessTab.Group>
+    )
+}
+interface TabListProps{
+    children?:React.ReactNode;
+}
+
+export function TabList({children}:TabListProps){
+    return(
+        <HeadlessTab.List className="border-b border-neutral-200 text-neutral-400">
+            {children}
+        </HeadlessTab.List>
+    )
+}
+
+interface TabProps{
+    children?:React.ReactNode;
+}
+export function Tab({children}:TabProps){
+    
+   return (    
+    <HeadlessTab  className={"px-2 py-1 hover:bg-blue-50"}>
+            {children}
+        </HeadlessTab>
+    )
+}
+
+interface TabPanelsProps{
+    children?:React.ReactNode;
+}
+
+export function TabPanels({children}:TabPanelsProps){
+    return(
+        <HeadlessTab.Panels>
+            {children}
+        </HeadlessTab.Panels>
+    )
+}
+
+interface TabPanelProps{
+    children?:React.ReactNode;
+}
+
+export function TabPanel({children}:TabPanelProps){
+    return(
+        <HeadlessTab.Panel className={"p-2"}>
+            {children}
+        </HeadlessTab.Panel>
+    )
+}
+// const Tab = () => {
+//   return (
+//     <HeadlessTab.Group>
+//     <HeadlessTab.List>
+//       <HeadlessTab>Tab 1</HeadlessTab>
+//       <HeadlessTab>HeadlessTab 2</HeadlessTab>
+//       <HeadlessTab>HeadlessTab 3</HeadlessTab>
+//     </HeadlessTab.List>
+//     <HeadlessTab.Panels>
+//       <HeadlessTab.Panel>Content 1</HeadlessTab.Panel>
+//       <HeadlessTab.Panel>Content 2</HeadlessTab.Panel>
+//       <HeadlessTab.Panel>Content 3</HeadlessTab.Panel>
+//     </HeadlessTab.Panels>
+//   </HeadlessTab.Group>
+//   )
+// }
+
+// export default Tab
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
+import { Input } from "./components/ui/Input";
+import{Tab,TabGroup,TabList,TabPanels,TabPanel} from "./components/ui/Tab";
 import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
+
+
+     
    
-   
+      <TabGroup>
+        <TabList>
+          <Tab>Recent</Tab>
+          <Tab>Popular</Tab>
+          <Tab>Trending</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>Recent Content Here</TabPanel>
+          <TabPanel>Populr Content Here</TabPanel>
+          <TabPanel>Trending Content Here</TabPanel>
+        </TabPanels>
+      </TabGroup>
  
     </div>
   );
