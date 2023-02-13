e92917f6a543c3e4ebcb0e65750c7a4fbc7bc127
sangosteve <stevesango9@gmail.com>
2023-02-13 13:36:43 +0200

    Alert variants

--- app/components/ui/Alert.tsx
+++ app/components/ui/Alert.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { cva, VariantProps } from "class-variance-authority";
-
+import clsx from "clsx";
 const alertStyles = cva(
-  "flex items-start p-4 max-w-sm border border-neutral-400 -text-body",
+  "flex items-start p-4 max-w-sm border border-neutral-400 rounded-md -text-body",
   {
     variants: {
       intent: {
         info: "text-blue-500",
-        warning: "",
-        success: "",
-        error: "",
+        warning: "text-orange-500",
+        success: "text-green-500",
+        error: "text-red-500",
       },
       variant: {
         light: "",
@@ -17,6 +17,72 @@ const alertStyles = cva(
         outline: "",
       },
     },
+    compoundVariants: [
+      //Light
+      {
+        intent: "info",
+        variant: "light",
+        class: "bg-blue-50 border-0 text-neutral-900",
+      },
+      {
+        intent: "warning",
+        variant: "light",
+        class: "bg-orange-50 border-0 text-neutral-900",
+      },
+      {
+        intent: "success",
+        variant: "light",
+        class: "bg-green-50 border-0 text-neutral-900",
+      },
+      {
+        intent: "error",
+        variant: "light",
+        class: "bg-red-50 border-0 text-neutral-900",
+      },
+      //Filled
+
+      {
+        intent: "error",
+        variant: "filled",
+        class: "bg-red-500 border-0 text-white",
+      },
+      {
+        intent: "success",
+        variant: "filled",
+        class: "bg-green-500 border-0 text-white",
+      },
+      {
+        intent: "warning",
+        variant: "filled",
+        class: "bg-orange-500 border-0 text-white",
+      },
+      {
+        intent: "info",
+        variant: "filled",
+        class: "bg-blue-500 border-0 text-white",
+      },
+      //Outline
+      {
+        intent: "error",
+        variant: "outline",
+        class: "bg-transparent border-red-500",
+      },
+      {
+        intent: "success",
+        variant: "outline",
+        class: "bg-transparent border-green-500",
+      },
+      {
+        intent: "warning",
+        variant: "outline",
+        class: "bg-transparent border-orange-500",
+      },
+      {
+        intent: "info",
+        variant: "outline",
+        class: "bg-transparent border-blue-500",
+      },
+    ],
   }
 );
 
@@ -34,7 +100,7 @@ export const Alert = ({
         <div className="flex items-start justify-start text-body font-semibold">
           {title}
         </div>
-        <div className="mt-2">{children}</div>
+        <div className="mt-1">{children}</div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,14 @@ import { Info } from "lucide-react";
 export default function Home() {
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
-      <Alert icon={<Info size={16} />} title="System Error">
-        Lorem ipsum dolor sit amet. Et quos itaque cum corrupti ipsam vel earum
-        maiores est porro commodi cum nobis veritatis.
+      <Alert
+        intent="info"
+        variant="outline"
+        icon={<Info className="p-0 m-0" size={16} />}
+        title="System Error"
+      >
+        Et quos itaque cum corrupti ipsam vel earum maiores est porro commodi
+        cum nobis veritatis.
       </Alert>
     </div>
   );
