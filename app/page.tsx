ccbd1008c26eaeb96ce116ff3d4051af51b07648
sangosteve <stevesango9@gmil.com>
2023-03-07 00:14:26 +0200

    locl repo commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import styles from "./page.module.css";
 import { Info } from "lucide-react";
 import { Button } from "./components/ui/Button";
 import Select from "./components/ui/SelectTemplate";
+import { Input } from "./components/ui/Input";
 export default function Home() {
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
       <Select />
+      <Input placeholder="0.0"/>
     </div>
   );
 }
