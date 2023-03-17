e939cb19205511bd7350dee29663c708f6b02416
sangosteve <stevesango9@gmail.com>
2023-03-17 15:05:06 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,6 @@
 "use client";
 import React from "react";
-import Alert from "./components/ui/Alert";
-import Badge from "./components/ui/Badge";
-import styles from "./page.module.css";
-import { Info } from "lucide-react";
-import { Button } from "./components/ui/Button";
-import { Select } from "./components/ui/Select";
-import Grid from "./components/ui/Grid/Grid";
+
 export default function Home() {
-  return (
-    <div className="w-screen h-screen ">
-      <Grid>
-        <Grid.Item cols={4}>
-          <div className="bg-indigo-500 w-full">cols = 3</div>
-        </Grid.Item>
-        <Grid.Item cols={7}>
-          <div className="bg-green-500 w-full">cols = 8</div>
-        </Grid.Item>
-      </Grid>
-    </div>
-  );
+  return <div className="w-screen h-screen "></div>;
 }
